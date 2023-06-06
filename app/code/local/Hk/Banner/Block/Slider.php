dd9a324063a67921db1cab4a8975f101e4eb0e79
hiren khunt <khiren39@gmail.com>
2023-06-06 15:15:22 +0530

    some changes done with show only active banner on grid

--- app/code/local/Hk/Banner/Block/Slider.php
+++ app/code/local/Hk/Banner/Block/Slider.php
@@ -6,6 +6,8 @@ class Hk_Banner_Block_Slider extends Mage_Core_Block_Template
         $groupId = Mage::getStoreConfig('banner/banner/bannergroup');
         $collection = Mage::getModel('banner/banner')->getCollection();
         $collection->addFieldToFilter('group_id', $groupId);
+        $collection->addFieldToFilter('status', 1);
+
         return $collection;
     }
 }
