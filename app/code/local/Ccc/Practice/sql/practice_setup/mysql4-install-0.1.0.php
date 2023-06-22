8ac8d4a6b1010f20fc8da652d40145afe5bab460
hiren khunt <khiren39@gmail.com>
2023-06-22 18:07:52 +0530

    attribute add colot top and down

--- app/code/local/Ccc/Practice/sql/practice_setup/mysql4-install-0.1.0.php
+++ app/code/local/Ccc/Practice/sql/practice_setup/mysql4-install-0.1.0.php
@@ -1,27 +1,49 @@
 <?php
 
-die;
-$installer = $this;
+$installer = new Mage_Eav_Model_Entity_Setup('core_setup');
+
 $installer->startSetup();
- 
-/*
- * Create all entity tables
- */
-$installer->createEntityTables(
-    $this->getTable('practice/practice_entity')
+
+$attributeOptions = array(
+    'option_1' => 'Red',
+    'option_2' => 'Blue',
+    'option_3' => 'Green',
+    'option_4' => 'Yellow',
+    'option_5' => 'Black',
+    'option_6' => 'White',
+    'option_7' => 'Orange',
+    'option_8' => 'Purple',
+    'option_9' => 'Pink',
+    'option_10' => 'Gray',
 );
- 
-/*
- * Add Entity type
- */
-$installer->addEntityType('Ccc_Practice',Array(
-    'entity_model'          =>'practice/practice',
-    'attribute_model'       =>'',
-    'table'                 =>'practice/practice_entity',
-    'increment_model'       =>'',
-    'increment_per_store'   =>'0'
+
+$installer->addAttribute('catalog_product', 'bottom_color', array(
+    'type'          => 'int',
+    'input'         => 'select',
+    'label'         => 'Bottom Color',
+    'required'      => 0,
+    'group'         => 'General',
+    'sort_order'    => '100',
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'visible'       => true,
+    'user_defined'  => true,
+    'option'        => array(
+        'values' => $attributeOptions
+    )
 ));
- 
-$installer->installEntities();
- 
+
+$installer->addAttribute('catalog_product', 'top_color', array(
+    'type'          => 'int',
+    'input'         => 'select',
+    'label'         => 'Top Color',
+    'required'      => 0,
+    'group'         => 'General',
+    'sort_order'    => '100',
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'visible'       => true,
+    'user_defined'  => true,
+    'option'        => array(
+        'values' => $attributeOptions
+    )
+));
 $installer->endSetup();
